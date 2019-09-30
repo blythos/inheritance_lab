e9a25f4cc6c00778fc077520a958fce85c58f68f
Stephen Blythe <stephenblythe@gmail.com>
2019-09-30 16:02:04 +0100

    initial commit

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,14 @@
+package staff;
+
+public class Employee {
+
+    private String name;
+    private String niNo;
+    private double salary;
+
+    public Employee(String name, String niNo, double salary){
+        this.name = name;
+        this.niNo = niNo;
+        this.salary = salary;
+    }
+}
---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+import staff.Employee;
+
+public class EmployeeTest {
+
+    Employee employee;
+
+    @Before
+    public void before(){
+        employee = new Employee();
+    }
+}
