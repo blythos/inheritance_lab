61b232747f10ed9f22aeb19931cfec457f279275
Stephen Blythe <stephenblythe@gmail.com>
2019-09-30 16:34:11 +0100

    Adds tests for director

---
+++ src/main/java/staff/management/Director.java
@@ -0,0 +1,15 @@
+package staff.management;
+
+public class Director extends Manager {
+
+    private double budget;
+
+    public Director(String name, String niNo, double salary, String deptName, double budget) {
+        super(name, niNo, salary, deptName);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+}
---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.Employee;
+import staff.management.Director;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+
+    private Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Kev", "BA60606D", 120000, "Codeclan", 120000000);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Kev", director.getName());
+    }
+
+    @Test
+    public void canGetNiNo(){
+        assertEquals("BA60606D", director.getNiNo());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(120000, director.getSalary(), 0.1);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        director.giveRaise(15000);
+        assertEquals(135000, director.getSalary(), 0);
+    }
+
+    @Test
+    public void canGetBonus(){
+        assertEquals(1200, director.bonus(), 0.1);
+    }
+
+    @Test
+    public void canGetBudget() {
+        assertEquals(120000000, director.getBudget(), 0.1);
+    }
+}
